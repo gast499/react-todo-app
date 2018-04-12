342b835f47c1495f19ce38147d04a51b59bc2c4b
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-11 20:57:33 -0400

    Setup imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers/'
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+//registerServiceWorker();
