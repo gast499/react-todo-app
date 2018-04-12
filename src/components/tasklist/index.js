50ca9c8cc4670490e94a3eeb59b960cecde4cf10
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-11 20:28:56 -0400

    Added tasklist render method and setup return table architecture

--- src/components/tasklist/index.js
+++ src/components/tasklist/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 
 export default class TaskList extends React.Component{
 
-    
+    render(){
+        return(
+          <table>
+              <thead>
+                
+              </thead>
+
+              <tbody>
+
+              </tbody>
+          </table>
+        );
+    }
 }
