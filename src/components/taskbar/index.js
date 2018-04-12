100a4d65373e2a17a4848a432e6ebeed1b545957
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-11 22:29:55 -0400

    Created mapToDispatch function and button onclick method

--- src/components/taskbar/index.js
+++ src/components/taskbar/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
-export default class TaskBar extends React.Component{
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {addTask} from '../../actions/';
+class TaskBar extends React.Component{
     render(){
         return(
           <div>
               <input type="text" ref="task" placeholder="Add New Task" />
-              <button>Add Task</button>
+              <button onClick={()=>this.props.addTask(this.refs.task.value)}>Add Task</button>
           </div>
         );
     }
 
-}
+};
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({addTask}, dispatch);
+};
+
+export default connect(()=>{},)(TaskBar)
