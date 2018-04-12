6d45dfb00d686f7fc2dcc5b44dfcaff746ee0394
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-11 22:23:04 -0400

    Setup Provider

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import App from './App';
 import reducers from './reducers/'
 //import registerServiceWorker from './registerServiceWorker';
 
+let store = createStore(reducers)
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+    <Provider store={store}>
+        <App/>,
+    </Provider>
+document.getElementById('root')
+)
+;
 //registerServiceWorker();
